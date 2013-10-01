0425343108e98f6b2a77746fc45df663ad5d1df3
Adam Gandelman <adamg@canonical.com>
2013-10-01 13:49:44 -0700

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [[ -z "$CONFIG_BZR_REPO" ]] ; then
-  echo "ERROR: Cannot build charm. Must set BZR_CONFIG_REPO in environment."
+  echo "ERROR: Cannot build charm. Must set CONFIG_BZR_REPO in environment."
   exit 1
 fi
 
